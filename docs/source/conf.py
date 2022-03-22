9da8e17b533237ca090a4b9c30370e8bc35453e4
Filipe Brandão <fdabrandao@gmail.com>
2022-03-22 20:15:00 +0000

    Change title in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,7 +121,7 @@ html_context = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+html_title = "AMPL Colaboratory"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
